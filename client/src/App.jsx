5fe3c5bf218f7668e3ca9a0eebff7dd467523fa0
James <jamespond707@gmail.comm>
2026-01-16 09:33:46 +0700

    feat: Initialize a new React project with Vite and configure Tailwind CSS.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">{/* Start coding here */}</div>;
+  return (
+    <div className="App bg-blue-500 text-white p-4">
+      <h1 className="text-3xl font-bold">Tailwind CSS is installed!</h1>
+    </div>
+  );
 }
 
 export default App;
